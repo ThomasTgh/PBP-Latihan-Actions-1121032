18ca1083c65f079253f286e8b5c70f239dae6cf6
Thomas Teguh <thmstgh@gmail.com>
2023-03-23 19:02:37 +0700

    commit 2

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/praktikum/Modul_Action/controllers"
 )
 
 func main() {
 	e := echo.New()
+
+	e.GET("/users", controllers.GetUsers) //Read
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
